3cb1882e621458c19f1aaa52cf8335e48c4b17e0
Ahmad Maulana <ahmadmaulana.jihad@gmail.com>
2020-11-13 01:23:16 +0700

    Tambah Data Seed buat Cek History Pemesanan

--- database/seeds/A13DataPemesanansSeeder.php
+++ database/seeds/A13DataPemesanansSeeder.php
@@ -14,7 +14,11 @@ class A13DataPemesanansSeeder extends Seeder
         //insert data ke table data_pemesanans
 
         $datapemesanans = array(
-            array('id' => 1,'waktu_pesan' => '2020-10-15 06:49:58','invoicecetak_no' => 6486511,'total_harga' => 450000,'kodepembayaran_id' => 1,'pembayaranstatus_id' => 2,'user_id' => 1,'created_at' => '2020-10-15 06:49:58','updated_at' => '2020-10-15 06:49:58')
+            array('id' => 1,'waktu_pesan' => '2020-10-15 06:49:58','invoicecetak_no' => 6486511,'total_harga' => 450000,'kodepembayaran_id' => 1,'pembayaranstatus_id' => 1,'user_id' => 1,'created_at' => '2020-10-15 06:49:58','updated_at' => '2020-10-15 06:49:58'),
+            array('id' => 2,'waktu_pesan' => '2020-10-15 06:49:58','invoicecetak_no' => 6486511,'total_harga' => 450000,'kodepembayaran_id' => 2,'pembayaranstatus_id' => 2,'user_id' => 1,'created_at' => '2020-10-15 06:49:58','updated_at' => '2020-10-15 06:49:58'),
+            array('id' => 3,'waktu_pesan' => '2020-10-15 06:49:58','invoicecetak_no' => 6486511,'total_harga' => 450000,'kodepembayaran_id' => 3,'pembayaranstatus_id' => 1,'user_id' => 2,'created_at' => '2020-10-15 06:49:58','updated_at' => '2020-10-15 06:49:58'),
+            array('id' => 4,'waktu_pesan' => '2020-10-15 06:49:58','invoicecetak_no' => 6486511,'total_harga' => 450000,'kodepembayaran_id' => 4,'pembayaranstatus_id' => 2,'user_id' => 2,'created_at' => '2020-10-15 06:49:58','updated_at' => '2020-10-15 06:49:58')
+
         );
         
         DB::table('data_pemesanans')->insert($datapemesanans);
