3ef5f4c54ab4f116ef4fda3bc79ab3ac35d70787
Ahmad Maulana <ahmadmaulana.jihad@gmail.com>
2020-12-08 00:32:21 +0700

    DB Rev and Migration Tabel

---
+++ database/migrations/2020_10_14_073530_create_perjalanans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerjalanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perjalanans', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('waktu_berangkat', 0)->nullable();
+            $table->timestamp('waktu_tiba', 0)->nullable();
+            $table->foreignId('keretakelas_id')->constrained('data_kereta_kelas')->onDelete('cascade');
+            $table->foreignId('lokasi_berangkat_id')->constrained('data_stasiuns')->onDelete('cascade');
+            $table->foreignId('lokasi_tiba_id')->constrained('data_stasiuns')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perjalanans');
+    }
+}
